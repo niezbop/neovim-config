b39a24aa622c5a1afb0f25c3f9a548bd06ce7bc2
Paul Niezborala <p.niezborala@wewanttoknow.com>
2024-10-23 00:31:35 +0200

    Switch status line from mini.statusline to lualine

---
+++ lua/niezbop/plugins/lualine.lua
@@ -0,0 +1,12 @@
+return {
+	"nvim-lualine/lualine.nvim",
+	-- dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("lualine").setup({
+			options = {
+				icons_enabled = vim.g.have_nerd_font,
+				theme = "auto",
+			},
+		})
+	end,
+}
