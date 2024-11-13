ad5ccba980c1caefffa2ca45cf0cf8b371d8a1b7
Paul Niezborala <pauniezbo@gmail.com>
2024-11-13 15:11:20 +0100

    Setup filetypes to handle fastlane files

---
+++ lua/niezbop/filetypes.lua
@@ -0,0 +1,10 @@
+vim.filetype.add({
+	filename = {
+		-- fastlane specific
+		Appfile = "ruby",
+		Cryptexfile = "ruby",
+		Fastfile = "ruby",
+		Matchfile = "ruby",
+		Pluginfile = "ruby",
+	},
+})
