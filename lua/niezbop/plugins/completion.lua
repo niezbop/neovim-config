e6a3d503018f594a6d5cae207237344bd194b261
Paul Niezborala <pauniezbo@gmail.com>
2024-11-13 16:19:47 +0100

    Move completion mappings in config

--- lua/niezbop/plugins/completion.lua
+++ lua/niezbop/plugins/completion.lua
@@ -38,7 +38,22 @@ return {
 						vim.snippet.expand(args.body)
 					end,
 				},
-				mapping = cmp.mapping.preset.insert({}),
+				mapping = cmp.mapping.preset.insert({
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+				}),
 			})
 		end,
 	},
